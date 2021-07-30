9430628f15d039275bc40d5ff943dd93b0f586d5
dinleo <dinleo11@gmail.com>
2021-07-30 22:59:00 +0900

    정답참조

--- Programmers_Test/level_3/3_가장먼노드_그래프.py
+++ Programmers_Test/level_3/3_가장먼노드_그래프.py
@@ -1,39 +1,19 @@
 def solution(n, edge):
-    if n == 1:
-        return 0
-    if n == 2:
-        return 1
-    node = [[]] * (n + 1)
-    cost = [20000] * (n + 1)
-    cost[0] = 0
-    cost[1] = 0
-    edge.sort()
+    import collections
+    check = [1]+[0]*(n-1)
+    vec = [[]for i in range(n)]
+    que = collections.deque([1])
     for i in edge:
-        temp = node[i[0]].copy()
-        if i[1] not in temp:
-            temp.append(i[1])
-        node[i[0]] = temp
-        temp = node[i[1]].copy()
-        if i[0] not in temp:
-            temp.append(i[0])
-        node[i[1]] = temp
+        vec[i[0]-1].append(i[1])
+        vec[i[1]-1].append(i[0])
+    while que:
+        l = len(que)
+        for i in range(l):
+            q = que.popleft()
+            for e in vec[q-1]:
+                if check[e-1] == 0:
+                    check[e-1] = 1
+                    que.append(e)
+    return l
 
-    def check(j, c):
-        for i in node[j]:
-            if cost[i] > c + 1:
-                cost[i] = c + 1
-        for i in node[j]:
-            if cost[i] == c + 1:
-                check(i, c + 1)
-        return
-
-    check(1, 0)
-    for i in range(n + 1):
-        if cost[i] == 20000:
-            cost[i] = 0
-    max_cost = max(cost)
-    print(cost)
-    return cost.count(max_cost)
-
-
-print(solution(7, [[1, 2], [1, 3], [2, 3], [3, 5], [5, 7], [7, 1]]))
+print(solution(6,[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
