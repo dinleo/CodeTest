ff8e9577a3d62228e71b3bf7b4aabd0501e2ad9f
dinleo <dinleo11@gmail.com>
2021-08-05 00:36:01 +0900

    문자열압축

--- Programmers_Test/level_2/2_문자열압축.py
+++ Programmers_Test/level_2/2_문자열압축.py
@@ -1,79 +1,43 @@
-# 너무 중구난방한 코드 추후 복습
 def solution(s):
-    if len(s) == 1:
-        return 1
-    if len(s) == 2:
-        return 2
-    answer = len(case_n_is_1(s))
-    for n in range(2, len(s)):
+    answer = len(s)
+    for n in range(1,int(len(s)/2) + 1) :
+
+        a = [s[i:i+n] for i in range(0,len(s),n)]
         new_s = ""
-        ss = s
-        pattern = ss[:n]
+        pattern = a[0]
         count = 1
-        ss = ss[n:]
-        while 1:
-            if ss == "":
+        for i in range(1,len(a)):
+            if a[i] != pattern:
                 if count == 1:
                     new_s += pattern
-                    break
+                    pattern = a[i]
                 else:
                     new_s += str(count) + pattern
-                    break
-            if pattern == ss[:n]:
-                count += 1
-                ss = ss[n:]
-            elif count == 1:
-                new_s += pattern
-                pattern = ss[:n]
-                ss = ss[n:]
+                    pattern = a[i]
+                    count = 1
             else:
-                new_s += str(count) + pattern
-                pattern = ss[:n]
-                ss = ss[n:]
-                count = 1
+                count += 1
+        if count == 1:
+            new_s += pattern
+        else:
+            new_s += str(count) + pattern
 
-        answer = min(answer, len(new_s))
+        #print(n,new_s)
+        answer = min(answer,len(new_s))
+
     return answer
 
 
-def case_n_is_1(s):
-    new_s = ""
-    ss = s
-    pattern = ss[0]
-    ss = ss[1:]
-    count = 1
-    while 1:
-        if ss == "":
-            if count == 1:
-                new_s += pattern
-                break
-            else:
-                new_s += str(count) + pattern
-                break
-        if pattern == ss[0]:
-            count += 1
-            ss = ss[1:]
-        elif count == 1:
-            new_s += pattern
-            pattern = ss[0]
-            ss = ss[1:]
-        else:
-            new_s += str(count) + pattern
-            pattern = ss[0]
-            ss = ss[1:]
-            count = 1
-    return new_s
 
 
-print(solution("aa"), 2)
+
+#print(solution("aa"), 2)
 print(solution("aabbaccc"), 7)
 print(solution("ababcdcdababcdcd"), 9)
 print(solution("abcabcdede"), 8)
 print(solution("abcabcabcabcdededededede"), 14)
 print(solution("xababcdcdababcdcd"), 17)
-print(solution("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxz"),
-      5)
-
+print(solution("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxz"),5)
 print(solution("acacacbacacac" ),9)
 print(solution("acacacacacacbacacacacacac"), 9)
 print(solution("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), 4)
